a4260b8cd8af6f5fafb6acb66af58e72b8a9043e
Jaime Cascante <je.cascante10@uniandes.edu.co>
2023-03-01 17:29:26 -0500

    update metapopinferences working

--- metapop/misc.py
+++ metapop/misc.py
@@ -15,3 +15,22 @@ def amro2cute(amro):
         return "e_faecalis"
     elif amro=="ENTEROCOCCUS FAECIUM":
         return "e_faecium"
+
+
+def amro2title(amro):
+    if amro == 'ESCHERICHIA COLI':
+        return "E. coli"
+    elif amro == 'KLEBSIELLA PNEUMONIAE':
+        return "K. pneumoniae"
+    elif amro=="PSEUDOMONAS AERUGINOSA":
+        return "P. aeruginosa"
+    elif amro=="METHICILLIN-SUSCEPTIBLE STAPHYLOCOCCUS AUREUS":
+        return "MSSA"
+    elif amro=="METHICILLIN-RESISTANT STAPHYLOCOCCUS AUREUS":
+        return "MRSA"
+    elif amro=="STAPHYLOCOCCUS EPIDERMIDIS":
+        return "S. epidermidis"
+    elif amro=="ENTEROCOCCUS FAECALIS":
+        return "E. faecalis"
+    elif amro=="ENTEROCOCCUS FAECIUM":
+        return "E. faecium"
