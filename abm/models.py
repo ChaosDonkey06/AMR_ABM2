950aa79ca77ee34b3869ee7119245812ffe1f6c0
Jaime Cascante <je.cascante10@uniandes.edu.co>
2023-03-03 17:44:17 -0500

    update inference and scenarios with the abm

--- abm/models.py
+++ abm/models.py
@@ -18,42 +18,31 @@ def amr_abm(t, agents_state, gamma, beta, alpha, movement, ward2size, model_sett
         parameters   : dictionary of parameters, contains importation rate (gamma), nosocomial transmission rate (beta),
                         effective sensitivity (ro), and decolonization rate (alpha)
     """
-    agents_state = np.array(agents_state)
 
     n  = model_settings["n"] # number of patients
     m  = model_settings["m"] # number of ensembles
 
-    # Decolonize patients according to. P(C2S) = α
-    #agents_state.at[:].set(np.maximum(agents_state - agents_state * (random.uniform(key=key, shape=(Np, )) < α), 0))
-    #agents_state = agents_state - agents_state * (np.random.uniform(size=(Np, )) < α)
     p_update = agents_state.copy()
     p_update = Patient.susceptible * (agents_state * np.random.random(size=(n, m)) <= alpha)
 
-    # import patients.
     new_patients = movement[movement["first_day"]==1]["mrn_id"].values
     if new_patients.shape[0] > 0:
-        # P(S2C) = \gamma - Probability of colonization given importation.
         p_update[new_patients, :] = Patient.colonized * (np.random.random(size=(new_patients.shape[0], m)) <= gamma)
 
-    # Compute force of infection for each ward.
     for i, ward_id in enumerate(movement["ward_id"].unique()):
         patients_ward = movement[movement["ward_id"]==ward_id]["mrn_id"].values
-
-        # λ_i = β  * C / N  - Force of infection for ward i.
         λ_i = beta * np.sum(p_update[patients_ward, :]==Patient.colonized) / ward2size[ward_id]
-
-        # P(C2S)_i = λ_i, we add the state but if already colonized the state would be 2 so we clip it to 1.
         p_update[patients_ward, :] = p_update[patients_ward, :] + Patient.colonized * (np.random.random(size=(patients_ward.shape[0], m)) <= λ_i)
     p_update = np.clip(p_update, 0, 1)
     return p_update
 
-def observe_cluster(t, patients_state, movement, rho, model_settings):
+def observe_cluster(t, agents_state, movement, rho, model_settings):
     ρ      = rho                  # effective sensitivity.
     k      = model_settings["k"]  # number of observations / clusters / buildings
     m      = model_settings["m"]  # number of clusters
 
     cluster_positive  = np.zeros((k, m))
-    p_test            = Observed.yes * (np.random.random(size=(patients_state.shape[0], m)) <= patients_state * ρ)
+    p_test            = Observed.yes * (np.random.random(size=(agents_state.shape[0], m)) <= agents_state * ρ)
     for i, cluster in enumerate(movement["cluster"].unique()):
         patients_test_ward            = movement.query(f"cluster=={cluster} and test==True")["mrn_id"].values
         cluster_positive[cluster,  :] = np.sum(p_test[patients_test_ward, :]    == Observed.yes, axis=0)
