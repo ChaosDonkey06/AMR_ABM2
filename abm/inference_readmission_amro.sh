863c111a01cf8f00f0fa435baf2c6c21fdb07805
Jaime Cascante Vega <je.cascante10@uniandes.edu.co>
2024-03-28 13:04:58 -0400

    update scriopt

--- abm/inference_readmission_amro.sh
+++ abm/inference_readmission_amro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=6
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=8:00:00
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jc12343@nyu.edu
 #SBATCH --output=slurm_%j.out
-#SBATCH --array=0-6
+#SBATCH --array=1-7
 
 module load python/intel/3.8.6
 
@@ -19,4 +19,4 @@ cd $RUNDIR
 PARRAY=(0 1 2 3 4 5 6)
 
 ## Execute the desired Stata do file script
-python3 InferenceReadmission_HPC.py --amro_idx $(SLURM_ARRAY_TASK_ID)
+python3 InferenceReadmission_HPC.py --amro_idx $SLURM_ARRAY_TASK_ID
